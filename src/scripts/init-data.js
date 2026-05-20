a9c09328a5f637ca301b3237b114923f2ad34834
JERSONLEIBOY <jersonleiboy@github.com>
2026-05-20 18:23:04 +0800

    feat: 修复已知问题

--- src/scripts/init-data.js
+++ src/scripts/init-data.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 const { config, logger } = require('../config');
 const User = require('../models/User');
 const Role = require('../models/Role');
@@ -16,11 +15,10 @@ const initData = async () => {
 
     await clearExistingData();
 
-    const roles = await createRoles();
-    const menus = await createMenus();
     const organizations = await createOrganizations();
-    const dictionaries = await createDictionaries();
-
+    await createDictionaries();
+    const menus = await createMenus();
+    const roles = await createRoles(menus);
     await createUsers(roles, organizations);
 
     logger.info('Data initialization completed successfully');
@@ -32,146 +30,15 @@ const initData = async () => {
 };
 
 const clearExistingData = async () => {
-  await User.deleteMany({});
-  await Role.deleteMany({});
-  await Menu.deleteMany({});
-  await Organization.deleteMany({});
-  await Dictionary.deleteMany({});
-  await DictionaryItem.deleteMany({});
-  logger.info('Existing data cleared');
-};
-
-const createRoles = async () => {
-  const roles = await Role.insertMany([
-    {
-      name: '管理员',
-      code: 'ADMIN',
-      remark: '系统管理员，拥有所有权限',
-      permissions: [],
-      status: 'active'
-    },
-    {
-      name: '普通用户',
-      code: 'USER',
-      remark: '普通用户，拥有基础权限',
-      permissions: [],
-      status: 'active'
-    },
-    {
-      name: '游客',
-      code: 'GUEST',
-      remark: '访客，仅有查看权限',
-      permissions: [],
-      status: 'active'
-    }
+  await Promise.all([
+    User.deleteMany({}),
+    Role.deleteMany({}),
+    Menu.deleteMany({}),
+    Organization.deleteMany({}),
+    Dictionary.deleteMany({}),
+    DictionaryItem.deleteMany({})
   ]);
-  logger.info(`Created ${roles.length} roles`);
-  return roles;
-};
-
-const createMenus = async () => {
-  const systemMenu = await Menu.create({
-    name: '系统管理',
-    type: 'directory',
-    path: '/system',
-    sort: 1,
-    visible: true
-  });
-
-  const userMenu = await Menu.create({
-    name: '用户管理',
-    type: 'menu',
-    path: '/system/user',
-    parentId: systemMenu._id,
-    sort: 1,
-    icon: 'user',
-    visible: true,
-    component: '/system/user/index'
-  });
-
-  const roleMenu = await Menu.create({
-    name: '角色管理',
-    type: 'menu',
-    path: '/system/role',
-    parentId: systemMenu._id,
-    sort: 2,
-    icon: 'role',
-    visible: true,
-    component: '/system/role/index'
-  });
-
-  const menuMenu = await Menu.create({
-    name: '菜单管理',
-    type: 'menu',
-    path: '/system/menu',
-    parentId: systemMenu._id,
-    sort: 3,
-    icon: 'menu',
-    visible: true,
-    component: '/system/menu/index'
-  });
-
-  const orgMenu = await Menu.create({
-    name: '机构管理',
-    type: 'menu',
-    path: '/system/organization',
-    parentId: systemMenu._id,
-    sort: 4,
-    icon: 'organization',
-    visible: true,
-    component: '/system/organization/index'
-  });
-
-  const dictMenu = await Menu.create({
-    name: '字典管理',
-    type: 'menu',
-    path: '/system/dictionary',
-    parentId: systemMenu._id,
-    sort: 5,
-    icon: 'dictionary',
-    visible: true,
-    component: '/system/dictionary/index'
-  });
-
-  const fileMenu = await Menu.create({
-    name: '文件管理',
-    type: 'menu',
-    path: '/system/file',
-    parentId: systemMenu._id,
-    sort: 6,
-    icon: 'file',
-    visible: true,
-    component: '/system/file/index'
-  });
-
-  const loginLogMenu = await Menu.create({
-    name: '登录日志',
-    type: 'menu',
-    path: '/system/login-record',
-    parentId: systemMenu._id,
-    sort: 7,
-    icon: 'login-log',
-    visible: true,
-    component: '/system/login-record/index'
-  });
-
-  const operationLogMenu = await Menu.create({
-    name: '操作日志',
-    type: 'menu',
-    path: '/system/operation-record',
-    parentId: systemMenu._id,
-    sort: 8,
-    icon: 'operation-log',
-    visible: true,
-    component: '/system/operation-record/index'
-  });
-
-  await Role.findByIdAndUpdate((await Role.findOne({ code: 'ADMIN' }))._id, {
-    permissions: [userMenu._id, roleMenu._id, menuMenu._id, orgMenu._id, dictMenu._id, fileMenu._id, loginLogMenu._id, operationLogMenu._id]
-  });
-
-  logger.info('Created system management menus');
-  return [systemMenu, userMenu, roleMenu, menuMenu, orgMenu, dictMenu, fileMenu, loginLogMenu, operationLogMenu];
+  logger.info('Existing data cleared');
 };
 
 const createOrganizations = async () => {
@@ -196,97 +63,159 @@ const createOrganizations = async () => {
   });
 
   logger.info('Created organization structure');
-  return [company, techDept, devTeam];
+  return { company, techDept, devTeam };
 };
 
 const createDictionaries = async () => {
-  const sexDict = await Dictionary.create({
-    dictName: '用户性别',
-    dictCode: 'sex',
-    sortNumber: 1
-  });
-
+  const sexDict = await Dictionary.create({ dictName: '用户性别', dictCode: 'sex', sortNumber: 1 });
   await DictionaryItem.insertMany([
     { dictId: sexDict._id, dictCode: 'sex', dictDataName: '男', dictDataCode: '1', sortNumber: 1 },
     { dictId: sexDict._id, dictCode: 'sex', dictDataName: '女', dictDataCode: '2', sortNumber: 2 }
   ]);
 
-  const orgTypeDict = await Dictionary.create({
-    dictName: '机构类型',
-    dictCode: 'organization_type',
-    sortNumber: 2
-  });
-
+  const orgTypeDict = await Dictionary.create({ dictName: '机构类型', dictCode: 'organization_type', sortNumber: 2 });
   await DictionaryItem.insertMany([
     { dictId: orgTypeDict._id, dictCode: 'organization_type', dictDataName: '公司', dictDataCode: '1', sortNumber: 1 },
     { dictId: orgTypeDict._id, dictCode: 'organization_type', dictDataName: '部门', dictDataCode: '2', sortNumber: 2 },
     { dictId: orgTypeDict._id, dictCode: 'organization_type', dictDataName: '小组', dictDataCode: '3', sortNumber: 3 }
   ]);
 
-  const statusDict = await Dictionary.create({
-    dictName: '状态',
-    dictCode: 'status',
-    sortNumber: 3
-  });
-
+  const statusDict = await Dictionary.create({ dictName: '状态', dictCode: 'status', sortNumber: 3 });
   await DictionaryItem.insertMany([
-    { dictId: statusDict._id, dictCode: 'status', dictDataName: '正常', dictDataCode: '1', sortNumber: 1 },
-    { dictId: statusDict._id, dictCode: 'status', dictDataName: '停用', dictDataCode: '2', sortNumber: 2 },
-    { dictId: statusDict._id, dictCode: 'status', dictDataName: '锁定', dictDataCode: '3', sortNumber: 3 }
+    { dictId: statusDict._id, dictCode: 'status', dictDataName: '正常', dictDataCode: '0', sortNumber: 1 },
+    { dictId: statusDict._id, dictCode: 'status', dictDataName: '冻结', dictDataCode: '1', sortNumber: 2 }
   ]);
 
   logger.info('Created dictionaries and items');
-  return [sexDict, orgTypeDict, statusDict];
+};
+
+const createMenus = async () => {
+  const systemMenu = await Menu.create({
+    title: '系统管理',
+    menuType: 0,
+    path: '/system',
+    parentId: '0',
+    sortNumber: 1,
+    icon: 'SettingOutlined',
+    hide: 0,
+    openType: 0
+  });
+
+  const pid = systemMenu._id.toString();
+
+  const [userMenu, roleMenu, menuMenu, orgMenu, dictMenu, fileMenu, loginLogMenu, operationLogMenu] =
+    await Menu.insertMany([
+      { title: '用户管理', menuType: 1, path: '/system/user',             parentId: pid, sortNumber: 1, icon: 'UserOutlined',     hide: 0, openType: 0, component: '/system/user' },
+      { title: '角色管理', menuType: 1, path: '/system/role',             parentId: pid, sortNumber: 2, icon: 'IdcardOutlined',   hide: 0, openType: 0, component: '/system/role' },
+      { title: '菜单管理', menuType: 1, path: '/system/menu',             parentId: pid, sortNumber: 3, icon: 'AppstoreOutlined', hide: 0, openType: 0, component: '/system/menu' },
+      { title: '机构管理', menuType: 1, path: '/system/organization',     parentId: pid, sortNumber: 4, icon: 'CityOutlined',     hide: 0, openType: 0, component: '/system/organization' },
+      { title: '字典管理', menuType: 1, path: '/system/dictionary',       parentId: pid, sortNumber: 5, icon: 'BookOutlined',     hide: 0, openType: 0, component: '/system/dictionary' },
+      { title: '文件管理', menuType: 1, path: '/system/file',             parentId: pid, sortNumber: 6, icon: 'FolderOutlined',   hide: 0, openType: 0, component: '/system/file' },
+      { title: '登录日志', menuType: 1, path: '/system/login-record',     parentId: pid, sortNumber: 7, icon: 'CalendarOutlined', hide: 0, openType: 0, component: '/system/login-record' },
+      { title: '操作日志', menuType: 1, path: '/system/operation-record', parentId: pid, sortNumber: 8, icon: 'LogOutlined',      hide: 0, openType: 0, component: '/system/operation-record' }
+    ]);
+
+  // 按钮级菜单（menuType: 2），authority 供前端权限指令使用
+  const buttons = await Menu.insertMany([
+    // 用户管理
+    { title: '查询用户',   menuType: 2, openType: 0, parentId: userMenu._id.toString(),      sortNumber: 1, authority: 'sys:user:list' },
+    { title: '添加用户',   menuType: 2, openType: 0, parentId: userMenu._id.toString(),      sortNumber: 2, authority: 'sys:user:save' },
+    { title: '修改用户',   menuType: 2, openType: 0, parentId: userMenu._id.toString(),      sortNumber: 3, authority: 'sys:user:update' },
+    { title: '删除用户',   menuType: 2, openType: 0, parentId: userMenu._id.toString(),      sortNumber: 4, authority: 'sys:user:remove' },
+    { title: '用户详情',   menuType: 1, openType: 0, path: '/system/user/details/:id', parentId: userMenu._id.toString(),      sortNumber: 4, icon: 'UserOutlined', hide: 1, component: '/system/user/details' },
+    // 角色管理
+    { title: '查询角色',   menuType: 2, openType: 0, parentId: roleMenu._id.toString(),      sortNumber: 1, authority: 'sys:role:list' },
+    { title: '添加角色',   menuType: 2, openType: 0, parentId: roleMenu._id.toString(),      sortNumber: 2, authority: 'sys:role:save' },
+    { title: '修改角色',   menuType: 2, openType: 0, parentId: roleMenu._id.toString(),      sortNumber: 3, authority: 'sys:role:update' },
+    { title: '删除角色',   menuType: 2, openType: 0, parentId: roleMenu._id.toString(),      sortNumber: 4, authority: 'sys:role:remove' },
+    // 菜单管理
+    { title: '查询菜单',   menuType: 2, openType: 0, parentId: menuMenu._id.toString(),      sortNumber: 1, authority: 'sys:menu:list' },
+    { title: '添加菜单',   menuType: 2, openType: 0, parentId: menuMenu._id.toString(),      sortNumber: 2, authority: 'sys:menu:save' },
+    { title: '修改菜单',   menuType: 2, openType: 0, parentId: menuMenu._id.toString(),      sortNumber: 3, authority: 'sys:menu:update' },
+    { title: '删除菜单',   menuType: 2, openType: 0, parentId: menuMenu._id.toString(),      sortNumber: 4, authority: 'sys:menu:remove' },
+    // 机构管理
+    { title: '查询机构',   menuType: 2, openType: 0, parentId: orgMenu._id.toString(),       sortNumber: 1, authority: 'sys:org:list' },
+    { title: '添加机构',   menuType: 2, openType: 0, parentId: orgMenu._id.toString(),       sortNumber: 2, authority: 'sys:org:save' },
+    { title: '修改机构',   menuType: 2, openType: 0, parentId: orgMenu._id.toString(),       sortNumber: 3, authority: 'sys:org:update' },
+    { title: '删除机构',   menuType: 2, openType: 0, parentId: orgMenu._id.toString(),       sortNumber: 4, authority: 'sys:org:remove' },
+    // 字典管理
+    { title: '查询字典',   menuType: 2, openType: 0, parentId: dictMenu._id.toString(),      sortNumber: 1, authority: 'sys:dict:list' },
+    { title: '添加字典',   menuType: 2, openType: 0, parentId: dictMenu._id.toString(),      sortNumber: 2, authority: 'sys:dict:save' },
+    { title: '修改字典',   menuType: 2, openType: 0, parentId: dictMenu._id.toString(),      sortNumber: 3, authority: 'sys:dict:update' },
+    { title: '删除字典',   menuType: 2, openType: 0, parentId: dictMenu._id.toString(),      sortNumber: 4, authority: 'sys:dict:remove' },
+    // 文件管理
+    { title: '上传文件',   menuType: 2, openType: 0, parentId: fileMenu._id.toString(),      sortNumber: 2, authority: 'sys:file:upload' },
+    { title: '删除文件',   menuType: 2, openType: 0, parentId: fileMenu._id.toString(),      sortNumber: 4, authority: 'sys:file:remove' },
+    { title: '查看记录',   menuType: 2, openType: 0, parentId: fileMenu._id.toString(),      sortNumber: 3, authority: 'sys:file:list' },
+  ]);
+
+  logger.info(`Created menus (${9 + buttons.length} total)`);
+  return { systemMenu, userMenu, roleMenu, menuMenu, orgMenu, dictMenu, fileMenu, loginLogMenu, operationLogMenu, buttons };
+};
+
+const createRoles = async (menus) => {
+  const pageMenuIds = [
+    menus.systemMenu._id, menus.userMenu._id, menus.roleMenu._id, menus.menuMenu._id,
+    menus.orgMenu._id, menus.dictMenu._id, menus.fileMenu._id,
+    menus.loginLogMenu._id, menus.operationLogMenu._id
+  ];
+  const buttonIds = menus.buttons.map(b => b._id);
+  const allMenuIds = [...pageMenuIds, ...buttonIds];
+
+  const [admin, user, guest] = await Role.insertMany([
+    { roleName: '管理员', roleCode: 'ADMIN', comments: '系统管理员，拥有所有权限', menus: allMenuIds },
+    { roleName: '普通用户', roleCode: 'USER',  comments: '普通用户，拥有基础权限', menus: [menus.systemMenu._id, menus.userMenu._id] },
+    { roleName: '游客',   roleCode: 'GUEST', comments: '访客，仅有查看权限',     menus: [menus.systemMenu._id] }
+  ]);
+
+  logger.info('Created roles');
+  return { admin, user, guest };
 };
 
 const createUsers = async (roles, organizations) => {
-  const adminRole = roles.find(r => r.code === 'ADMIN');
-  const userRole = roles.find(r => r.code === 'USER');
-  const guestRole = roles.find(r => r.code === 'GUEST');
-  const devTeam = organizations.find(o => o.organizationName === '开发组');
-
-  const hashedAdminPassword = await bcrypt.hash('admin123', 10);
-  const hashedTestPassword = await bcrypt.hash('test123', 10);
-  const hashedGuestPassword = await bcrypt.hash('guest123', 10);
-
-  await User.insertMany([
+  // 密码由 User.pre('save') 钩子自动 bcrypt 哈希，不需要手动 hash
+  const users = [
     {
       username: 'admin',
-      name: '管理员',
-      sex: 'unknown',
+      nickname: '管理员',
       email: 'admin@example.com',
       phone: '13800138000',
-      password: hashedAdminPassword,
-      status: 'active',
-      remark: '系统管理员账号',
-      organization: devTeam ? devTeam._id : null,
-      roles: [adminRole._id]
+      password: 'admin123',
+      status: 0,
+      comments: '系统管理员账号',
+      organizationId: organizations.devTeam._id,
+      roles: [roles.admin._id]
     },
     {
       username: 'testuser',
-      name: '测试用户',
-      sex: 'male',
+      nickname: '测试用户',
+      sex: '1',
       email: 'test@example.com',
       phone: '13800138001',
-      password: hashedTestPassword,
-      status: 'active',
-      remark: '测试用户账号',
-      organization: devTeam ? devTeam._id : null,
-      roles: [userRole._id]
+      password: 'test123',
+      status: 0,
+      comments: '测试用户账号',
+      organizationId: organizations.devTeam._id,
+      roles: [roles.user._id]
     },
     {
       username: 'guestuser',
-      name: '访客用户',
-      sex: 'female',
+      nickname: '访客用户',
+      sex: '2',
       email: 'guest@example.com',
       phone: '13800138002',
-      password: hashedGuestPassword,
-      status: 'active',
-      remark: '访客账号',
-      organization: devTeam ? devTeam._id : null,
-      roles: [guestRole._id]
+      password: 'guest123',
+      status: 0,
+      comments: '访客账号',
+      organizationId: organizations.devTeam._id,
+      roles: [roles.guest._id]
     }
-  ]);
+  ];
+
+  // insertMany 不触发 pre('save') 钩子，需逐条 save 以触发密码哈希
+  for (const data of users) {
+    await new User(data).save();
+  }
 
   logger.info('Created users: admin, testuser, guestuser');
 };
